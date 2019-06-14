e48f8c6bc4f690f3a4b5a5f5dd19b836826b8565
monell <monell.zhong@gmail.com>
2019-06-14 23:10:44 +0800

    obj init

--- meshsim/he_mesh.hpp
+++ meshsim/he_mesh.hpp
@@ -2,50 +2,72 @@
 #define _HE_MESH_
 
 #include "linear.hpp"
+namespace he {
 
+struct Edge;
 
-struct he_vert {
+struct Vert {
     V3 pos;
-    he_edge* edge;
+    Edge* edge; //这条边以当前v为起点(v[0])
     M4 error;
     int id;
-    he_vert():edge(nullptr),id(-1){}
+    Vert():edge(nullptr),id(-1){}
+};
 
-    
-    bool calculateMat() {
-        he_edge* e = edge;
-        do {
-            error += M4(e->face->p);
-            e = e->pair->next;
+struct Face {
+    Edge* edge; //面上的半边之一
+    V4 p; //平面参数 ax + by + cz + d = 0 p = (a,b,c,d)  a^2 + b^2 + c^2 = 1
+    int id;
+    Face():edge(nullptr),id(-1){}
+};
 
-            assert(e != nullptr);
-        } while (e != edge);
+
+struct Edge {
+    int id;
+    Vert* v[2]; //半边对应的顶点 v[0]->v[1]
+    Edge* pair; //对应的半边
+    Face* face; //对应的面
+    Edge* next; //在该面上的下一条半边
+    Edge* prev; //上一条半边
+    Edge():pair(nullptr),face(nullptr),next(nullptr),prev(nullptr),id(-1) {
+        v[0] = v[1] = nullptr;
     }
 };
 
-struct he_face {
-    he_edge* edge; //面上的半边之一
-    V4 p; //平面参数 ax + by + cz + d = 0 p = (a,b,c,d)  a^2 + b^2 + c^2 = 1
-};
-
-
-struct he_edge {
-    he_vert* v[2]; //半边对应的顶点 v[0]->v[1]
-    he_edge* pair; //对应的半边
-    he_face* face; //对应的面
-    he_edge* next; //在该面上的下一条半边
-    he_edge* prev; //上一条半边
-};
+typedef std::pair<Vert*, Vert*> stdPair;
 
 struct Pair {
-    he_edge* edge;
+    Edge* edge; //若edge == nullptr　代表两个点之间之前未链接
+    Vert* v[2];
 
     V3 bestPos;
     double cost;
 
-    Pair(he_edge* e):edge(e),cost(INF) {
+    Pair(Edge* e):edge(e),cost(INF) {
+        v[0] = e->v[0];
+        v[1] = e->v[1];
         calculateBestPoint();
     }
+
+    Pair(Vert* v0,Vert* v1):edge(nullptr),cost(INF) {
+        v[0] = v0;
+        v[1] = v1;
+        calculateBestPoint();
+    }
+
+    Pair(const Pair& p) {
+        edge = p.edge;
+        bestPos = p.bestPos;
+        cost = p.cost;
+    }
+
+    void printInfo() const {
+        std::cout << "edge: " << edge << std::endl;
+        std::cout << "v[0]xyz: " << v[0]->pos[0] << ", " << v[0]->pos[1] << ", " << v[0]->pos[2] << std::endl;
+        std::cout << "v[1]xyz: " << v[1]->pos[0] << ", " << v[1]->pos[1] << ", " << v[1]->pos[2] << std::endl;
+        std::cout << "cost: " << cost << std::endl;
+    }
+
     void updateCost(double new_cost, const V3& new_pos) {
         if (new_cost < cost) {
             cost = new_cost;
@@ -53,29 +75,32 @@ struct Pair {
         }
     }
 
+    void recalculate() {
+        cost = INF;
+        calculateBestPoint();
+    }
     void calculateBestPoint() {
-        he_vert *v0 = edge->v[0], *v1 = edge->v[1];
         //确保之前已经计算好了顶点的error
-        M4 Q_ = v0->error + v1->error;
+        M4 Q_ = v[0]->error + v[1]->error;
         
         double det = Q_.det4Affine();
 
 
         //TODO: 需要比较这些吗?
-        double error_0 = Q_.quadric(v0->pos);
-        updateCost(error_0,v0->pos);
-        double error_1 = Q_.quadric(v1->pos);
-        updateCost(error_1,v1->pos);
-        double error_mid = Q_.quadric((v0->pos + v1->pos) / 2);
-        updateCost(error_mid,(v1->pos + v0->pos) / 2);
+        double error_0 = Q_.quadric(v[0]->pos);
+        updateCost(error_0,v[0]->pos);
+        double error_1 = Q_.quadric(v[1]->pos);
+        updateCost(error_1,v[1]->pos);
+        double error_mid = Q_.quadric((v[0]->pos + v[1]->pos) / 2);
+        updateCost(error_mid,(v[1]->pos + v[0]->pos) / 2);
 
         if (fabs(det) < EPS) { 
             //不可逆
             //考虑v0 + (v1 - v0) t,  t \in [0,1]线段
-            double denominator = Q_.quadric(v0->pos - v1->pos);
+            double denominator = Q_.quadric(v[0]->pos - v[1]->pos);
             if (fabs(denominator) > EPS) {
-                double t = Q_.quadric(v0->pos,v0->pos - v1->pos) / denominator;
-                V3 pos = v0->pos + (v1->pos - v0->pos) * t;
+                double t = Q_.quadric(v[0]->pos,v[0]->pos - v[1]->pos) / denominator;
+                V3 pos = v[0]->pos + (v[1]->pos - v[0]->pos) * t;
                 double error = Q_.quadric(pos);
                 updateCost(error, pos);
             }
@@ -89,10 +114,18 @@ struct Pair {
         
     }
 
+    void update2opti() {
+        //把v0更新到最优点
+        v[0]->pos = bestPos;
+        v[0]->error += v[1]->error;
+        //外面进行边删除
+    }
+
 };
 
-inline he_edge* is_edge(he_vert* v0, he_vert* v1) {
-    he_edge* edge = v0->edge;
+inline Edge* is_edge(Vert* v0, Vert* v1) {
+    //找到一条v0指向v1的边
+    Edge* edge = v0->edge;
     if (!edge) return nullptr;
     do {
         if (edge->v[1] == v1) return edge;
@@ -101,29 +134,255 @@ inline he_edge* is_edge(he_vert* v0, he_vert* v1) {
     return nullptr;
 }
 
+inline void calculateMat4Vert(Vert* v) {
+    Edge* e = v->edge;
+    do {
+        v->error += M4(e->face->p);
+        e = e->pair->next;
+
+        assert(e != nullptr);
+    } while (e != v->edge);
+}
+
 struct Mesh {
-    std::vector<he_vert> verts;
-    std::vector<he_edge> edges;
-    std::vector<he_face> faces;
+    std::vector<Vert*> verts;
+    std::vector<Edge*> edges;
+    //面都是三角片!!
+    std::vector<Face*> faces;
+
+    std::vector<bool> vertEnable;
+    std::vector<bool> edgeEnable;
+    std::vector<bool> faceEnable;
+
+    int vertCount = 0;
+    int edgeCount = 0;
+    int faceCount = 0;
     
-    void addVert(he_vert& v) {
-        v.id = verts.size();
+    void addVert(Vert* v) {
+        v->id = verts.size();
         verts.push_back(v);
+        vertEnable.push_back(true);
+        vertCount++;
     }
-    void addEdge(const he_edge& e) {
+    void addEdge(Edge* e) {
+        e->id = edges.size();
         edges.push_back(e);
+        edgeEnable.push_back(true);
+        edgeCount++;
     }
-    void addFace(const he_face& f) {
+    void addFace(Face* f) {
+        f->id = faces.size();
         faces.push_back(f);
+        faceEnable.push_back(true);
+        faceCount++;
     }
 
     void calculateError() {
         //计算所有顶点的误差矩阵
-        for (int i = 0;i < verts.size(); ++i)
-            verts[i].calculateMat();
+        for (int i = 0;i < verts.size(); ++i) {
+            calculateMat4Vert(verts[i]);
+            //std::cout << "---" << std::endl;
+            //verts[i]->error.print();
+        }
+    }
+
+    void deleteEdge(Edge* edge) {
+        assert(edgeEnable[edge->id]);
+        //删除一条存在的边 v0->v1
+        //把edge->v[1]删除　留下edge->v[0]
+        //因此实际上就是删掉v[1]并重新链接
+        //将会删除两个三角面 6条半边(包括当前边) 1个顶点
+
+        Vert* v1 = edge->v[1];
+        Vert* v0 = edge->v[0];
+        //删除节点v[1]
+        vertEnable[v1->id] = false;
+        vertCount--;
+        //删除边
+        edgeEnable[edge->id] = false;
+        edgeEnable[edge->pair->id] = false;
+        //std::cout << "delete edge id: " << edge->id << std::endl;
+        //std::cout << "delete edge id: " << edge->pair->id << std::endl;
+        edgeCount -= 2;
+        /*
+            v_up
+          /      \
+        v0 --e--> v1
+           <----
+          \      /
+            v_down 
+        */
+        //所有非v0，　且与v0无边的邻居
+        //除此之外还有3个点(v0和两个与v0有边的顶点)
+        std::vector<Vert*> neighbor_vert;
+        std::vector<Edge*> neighbor_edge;
+
+        {
+            Edge* e = edge->pair;
+            assert(e->v[0] == v1);
+            assert(e->v[1] == v0);
+
+            do {
+                if (e->v[1] != v0 && !is_edge(e->v[1],v0)) {
+                    neighbor_vert.push_back(e->v[1]);
+                    neighbor_edge.push_back(e);
+                }
+
+                e = e->pair->next;
+            } while (e != edge->pair);
+        }
+
+        //std::cout << "vert neighbor: " << neighbor_vert.size() << std::endl;
+        //std::cout << "edge neighbor: " << neighbor_edge.size() << std::endl;
+
+
+        //检查与v0有边的两个顶点v_up,v_down
+        Vert* v_down = edge->next->v[1];
+        //e_down: v_down -> v0
+        Edge* e_down = edge->next->next; 
+        Vert* v_up = edge->pair->next->v[1];
+        //e_up: v0 -> v_up
+        Edge* e_up = edge->pair->next;
+        assert(e_down->v[0] == v_down && e_down->v[1] == v0);
+        assert(e_up->v[0] == v0 && e_up->v[1] == v_up);
+        //三角关系
+        assert(e_up->next->next->v[1] == v0);
+        assert(e_down->next->next->v[1] == v_down);
+
+        //更改v0 v_up v_down的edge　避免被删
+        v_up->edge = e_up->pair;
+        assert(v_up->edge->v[0] == v_up && v_up->edge->v[1] == v0);
+        v_down->edge = e_down;
+        assert(v_down->edge->v[0] == v_down && v_down->edge->v[1] == v0);        
+        v0->edge = v_down->edge->pair;
+        assert(v0->edge->v[0] == v0);
+
+        //删除v1->v_down和v_down->v1这两条边
+        {
+            assert(e_down->prev->v[0] == v1 && e_down->prev->v[1] == v_down);
+            edgeEnable[e_down->prev->id] = false;
+            edgeEnable[e_down->prev->pair->id] = false;
+            edgeCount -= 2;
+            //std::cout << "delete edge id: " << e_down->prev->id << std::endl;
+            //std::cout << "delete edge id: " << e_down->prev->pair->id << std::endl;
+
+            faceEnable[e_down->face->id] = false;
+            faceCount--;
+
+            //重新连接形成三角片
+            Edge* e_next = e_down->prev->pair->next;
+            Edge* e_next_next = e_down->prev->pair->next->next;
+            
+            e_down->next = e_next;
+            e_next->prev = e_down;
+            e_down->prev = e_next_next;
+            e_next_next->next = e_down;
+            
+            //面改变
+            e_down->face = e_next->face;
+
+            //检查三角关系
+            //std::cout << "e_down check "<< e_down->id << ", " << e_next->id << ", " << e_next_next->id << std::endl;
+            checkEdge(e_down);
+            //std::cout << "e_down check done" << std::endl;
+
+        }
+
+        //删除v1->v_up和v_up->v1这两条边
+        {
+            assert(e_up->next->v[1] == v1 && e_up->prev->v[1] == v0);
+            edgeEnable[e_up->next->id] = false;
+            edgeEnable[e_up->next->pair->id] = false;
+            edgeCount -= 2;
+            //std::cout << "delete edge id: " << e_up->next->id << std::endl;
+            //std::cout << "delete edge id: " << e_up->next->pair->id << std::endl;
+            faceEnable[e_up->face->id] = false;
+            faceCount--;
+
+            //重新连接三角片
+            Edge* e_next = e_up->next->pair->next;
+            Edge* e_next_next = e_up->next->pair->prev;
+            assert(e_next->next == e_next_next);
+
+            e_up->next = e_next;
+            e_next->prev = e_up;
+            e_up->prev = e_next_next;
+            e_next_next->next = e_up;
+
+            //面改变
+            e_up->face = e_next->face;
+
+            //检查三角关系
+            //std::cout << "e_up check "<< e_up->id << ", " << e_next->id << ", " << e_next_next->id << std::endl;
+            checkEdge(e_up);
+            //std::cout << "e_up check done" << std::endl;
+        }
+
+        //更新其余的顶点邻居        
+        {
+            for (int i = 0;i < neighbor_vert.size(); ++i) {
+                assert(neighbor_edge[i]->v[1] == neighbor_vert[i]);
+                assert(neighbor_edge[i]->v[0] == v1);
+                assert(neighbor_edge[i]->pair->v[1] == v1);
+                assert(neighbor_edge[i]->pair->v[0] == neighbor_vert[i]);
+
+                //将边的起点更改为v0  对应边的终点改为v0
+                neighbor_edge[i]->v[0] = v0;
+                neighbor_edge[i]->pair->v[1] = v0;
+            }
+        }
+    }
+
+    void mergeVert(Vert* v0, Vert* v1) {
+        //合并两个节点 这两个节点之间不存在边!
+    }
+
+
+    void checkTotal() {
+        //检查所有节点结构
+        for (int i = 0;i < verts.size(); ++i) {
+            if (vertEnable[i]) {
+                checkVert(verts[i]);
+            }
+        }
+    }
+
+    void checkVert(Vert* v) {
+        //检查这个顶点对应的边结构
+        Edge* e0 = v->edge, *e1 = e0->next, *e2 = e1->next;
+        
+        //检查顶点
+        assert(e0->v[1] == e1->v[0]);
+        assert(e1->v[1] == e2->v[0]);
+        assert(e2->v[1] == e0->v[0]);
+
+        Edge* e = v->edge;
+        do {
+            assert(e != nullptr);
+            checkEdge(e);
+            e = e->pair->next;
+        } while (e != v->edge);
+    }
+
+    void checkEdge(Edge* e) {
+        Edge *e0 = e, *e1 = e0->next, *e2 = e1->next;
+        assert(e0 != nullptr);
+        assert(e1 != nullptr);
+        assert(e2 != nullptr);
+        //检查三角结构
+        assert(e1->prev == e0);
+        assert(e2->prev == e1);
+        assert(e0->prev == e2);
+        assert(e0 == e2->next);
+
+        //检查面id
+        assert(e0->face->id == e1->face->id && e0->face->id == e2->face->id);
+        assert(e0->face->id != -1);
     }
 };
 
+
+}; //namespace he
 
 
 #endif
--- meshsim/objProcessor.hpp
+++ meshsim/objProcessor.hpp
@@ -14,16 +14,106 @@
 #define TINYOBJLOADER_USE_DOUBLE
 #include "tiny_obj_loader.h"
 
+
+
 struct ObjProcessor {
+    struct int2 {
+        int x,y;
+        int2(int x_ = -1,int y_ = -1):x(x_),y(y_){}
+        bool operator<(const int2& a) const {
+            if (x < a.x) return true;
+            else if (x == a.x && y < a.y) return true;
+            else return false;
+        }
+        bool operator==(const int2& a) const {return x == a.x && y == a.y;}
+        bool operator!=(const int2& a) const {return !(*this == a);}
+    };
+    struct int3 {
+        int x,y,z;
+        int3(int x_ = -1,int y_ = -1,int z_ = -1):x(x_),y(y_),z(z_){}
+        bool operator<(const int3& a) const {
+            if (x < a.x) return true;
+            else if (x == a.x && y < a.y) return true;
+            else if (x == a.x && y == a.y && z < a.z) return true;
+            else return false;
+        }
+        bool operator==(const int3& a) const {return x == a.x && y == a.y && z == a.z;}
+        bool operator!=(const int3& a) const {return !(*this == a);}
+    };
+
+
+    //void connectEdge(he::Mesh& mesh, std::map<int,int3>& face_vert3_map, std::map<int2,int2> vert2_faces_map, int face_id, const int2& order) {
+    void connectEdge(he::Mesh& mesh, std::vector<int3>& face_vert3_map, std::map<int2,int2>& vert2_faces_map, std::map<int2,int>& vert2_edge_map, int face_id, const int2& order) {
+        if (mesh.faces[face_id]->edge != nullptr) return;
+
+        int3 i3 = face_vert3_map[face_id];
+        int z;
+        if (i3.x != order.x && i3.x != order.y) z = i3.x;
+        else if (i3.y != order.x && i3.y != order.y) z = i3.y;
+        else z = i3.z;
+        static int i = 0;
+        //std::cout << "round: " << i++ << std::endl;
+        assert(z != order.x && z != order.y);
+
+        he::Edge *e0 = new he::Edge();
+        he::Edge *e1 = new he::Edge();
+        he::Edge *e2 = new he::Edge();
+
+        e0->face = e1->face = e2->face = mesh.faces[face_id];
+
+        e0->v[0] = mesh.verts[order.x];
+        e0->v[1] = mesh.verts[order.y];
+        e1->v[0] = mesh.verts[order.y];
+        e1->v[1] = mesh.verts[z];
+        e2->v[0] = mesh.verts[z];
+        e2->v[1] = mesh.verts[order.x];
+
+        if (mesh.verts[order.x]->edge == nullptr) mesh.verts[order.x]->edge = e0;
+        if (mesh.verts[order.y]->edge == nullptr) mesh.verts[order.y]->edge = e1;
+        if (mesh.verts[z]->edge == nullptr) mesh.verts[z]->edge = e2;
+
+        e0->next = e1;
+        e1->next = e2;
+        e2->next = e0;
+        e0->prev = e2;
+        e2->prev = e1;
+        e1->prev = e0;
+
+        mesh.addEdge(e0);
+        mesh.addEdge(e1);
+        mesh.addEdge(e2);
+
+        mesh.faces[face_id]->edge = e0;
+
+
+        vert2_edge_map[int2(e0->v[0]->id,e0->v[1]->id)] = e0->id;
+        vert2_edge_map[int2(e1->v[0]->id,e1->v[1]->id)] = e1->id;
+        vert2_edge_map[int2(e2->v[0]->id,e2->v[1]->id)] = e2->id;
+
+        //从e1 e2两边扩展
+        //e1
+        int2 new_order(z,order.y);
+        int2 faces_id = vert2_faces_map[int2(min(z,order.y),max(z,order.y))];
+        int new_face_id = faces_id.x == face_id? faces_id.y:faces_id.x;
+        connectEdge(mesh,face_vert3_map,vert2_faces_map,vert2_edge_map,new_face_id,new_order);
+
+        //e2
+        new_order = int2(order.x,z);
+        faces_id = vert2_faces_map[int2(min(z,order.x),max(z,order.x))];
+        new_face_id = faces_id.x == face_id? faces_id.y:faces_id.x;
+        connectEdge(mesh,face_vert3_map,vert2_faces_map,vert2_edge_map,new_face_id,new_order);
+    }
+
+
     bool checkFileName(const std::string &fn) {
         std::string format = fn.substr(fn.length() - 4, 4);
-        if (format == "obj") return true;
+        if (format == ".obj") return true;
         else {
             std::cout << "unvalid file format: " << format << std::endl;
             return false;
         }
     }
-    bool loadFile(Mesh& mesh,const std::string& fn) {
+    bool loadFile(he::Mesh& mesh,const std::string& fn) {
         if (!checkFileName(fn)) return false;
 
         tinyobj::attrib_t attrib;
@@ -53,23 +143,95 @@ struct ObjProcessor {
             return false;
         }
 
-        for (size_t s = 0; s < shapes.size(); s++) {
-            size_t index_offset = 0;
-            for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-                int fv = shapes[s].mesh.num_face_vertices[f];
+        std::map<int3,int> vert3_face_map;
+        std::map<int2,int2> vert2_face2_map;
+        std::map<int2,int> vert2_edge_map;
+        std::vector<int3> face_vert3_map;
 
-                for (size_t v = 0; v < fv; v++) {
-                    //do something
-                    //TODO
-                }
-                index_offset += fv;
-            }
+        for (int i = 0;i < attrib.vertices.size(); i += 3) {
+            he::Vert* v = new he::Vert();
+            tinyobj::real_t vx = attrib.vertices[i];
+            tinyobj::real_t vy = attrib.vertices[i + 1];
+            tinyobj::real_t vz = attrib.vertices[i + 2];
+            v->pos = V3(vx,vy,vz);
+            mesh.addVert(v);
+            assert(v->id == i / 3);
         }
-         
+
+        size_t index_offset = 0;
+        for (size_t f = 0; f < shapes[0].mesh.num_face_vertices.size(); f++) {
+            int fv = shapes[0].mesh.num_face_vertices[f];
+            assert(fv == 3);
+
+            he::Face* face = new he::Face();
+            mesh.addFace(face);
+
+            tinyobj::index_t idx[3];
+            for (int i = 0;i < 3; ++i) idx[i] = shapes[0].mesh.indices[index_offset + i];
+
+            int vert_index[3];
+            V3 v[3];
+            for (int i = 0;i < 3; ++i) {
+                vert_index[i] = idx[i].vertex_index;
+                v[i] = mesh.verts[vert_index[i]]->pos;
+            }
+
+            vert3_face_map[int3(vert_index[0],vert_index[1],vert_index[2])] = face->id;
+            face_vert3_map.push_back(int3(vert_index[0],vert_index[1],vert_index[2]));
+
+            for (int i = 0;i < 3; ++i) {
+                int min_index = min(vert_index[i],vert_index[(i + 1) % 3]);
+                int max_index = max(vert_index[i],vert_index[(i + 1) % 3]);
+
+                int2 i2(min_index,max_index);
+                auto itr = vert2_face2_map.find(i2);
+                if (itr == vert2_face2_map.end()) {
+                    int2 faces_id(face->id,-1);
+                    vert2_face2_map[i2] = faces_id;
+                } else if (itr->second.y == -1) {
+                    itr->second.y = face->id;
+                    assert(itr->second.x != face->id);
+                } else {
+                    std::cout << "??? error" << std::endl;
+                }
+            }            
+
+            V3 normal = (v[0] - v[1]) & (v[1] - v[2]);
+            normal = normal.norm();
+            double d = (-1) * (normal * v[0]);
+            face->p = V4(normal.x,normal.y,normal.z,d);
+
+            index_offset += fv;
+        }
+        int3 order = face_vert3_map[0];
+        connectEdge(mesh,face_vert3_map,vert2_face2_map,vert2_edge_map,0,int2(order.x,order.y));
+
+        //处理pair
+        for (int i = 0;i < mesh.edges.size(); ++i) {
+            int2 i2(mesh.edges[i]->v[1]->id,mesh.edges[i]->v[0]->id);
+            mesh.edges[i]->pair = mesh.edges[vert2_edge_map[i2]];
+        }
+        return true;
     }
 
 
-    bool dumpFile() {
+    void dumpFile(he::Mesh& mesh, std::string& fn) {
+        FILE* fp = fopen(fn.c_str(),"w");
+        std::map<int,int> idxMap;
+        for (int i = 0, k = 0;i < mesh.verts.size(); ++i) {
+            if (!mesh.vertEnable[i]) continue;
+            fprintf(fp,"v %f %f %f\n",mesh.verts[i]->pos[0],mesh.verts[i]->pos[1],mesh.verts[i]->pos[2]);
+            idxMap[mesh.verts[i]->id] = ++k;
+        }
+
+        for (int i = 0;i < mesh.faces.size(); ++i) {
+            if (!mesh.faceEnable[i]) continue;
+            he::Edge *e = mesh.faces[i]->edge;
+            he::Vert *v0 = e->v[0], *v1 = e->next->v[0], *v2 = e->next->next->v[0];
+            assert(v0 == e->next->next->next->v[0]);
+
+            fprintf(fp,"f %d %d %d\n",idxMap[v0->id],idxMap[v1->id],idxMap[v2->id]);
+        }
 
     }
 };
